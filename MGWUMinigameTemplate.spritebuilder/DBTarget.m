a42bb07d1cbe5939fe39f2dc20e9df12c475f175
djbyrne <donaljbyrne@me.com>
2014-07-25 11:23:56 +0100

    Part5.2
    made a small mistake in the last update

---
+++ MGWUMinigameTemplate.spritebuilder/DBTarget.m
@@ -0,0 +1,13 @@
+//
+//  DBTarget.m
+//  MGWUMinigameTemplate
+//
+//  Created by djb on 25/07/2014.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "DBTarget.h"
+
+@implementation DBTarget
+
+@end
